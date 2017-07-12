5cfaf0547ec8b9b2d72db88442616376df92a146
Reason.H.Duan <Reason.H.Duan@newegg.com>
2017-07-12 13:56:15 +0800

    update main function

--- CommonDemo/src/main/java/com/reason/App.java
+++ CommonDemo/src/main/java/com/reason/App.java
@@ -6,8 +6,8 @@ package com.reason;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
+        // main function
         System.out.println( "Hello World!" );
     }
 }
