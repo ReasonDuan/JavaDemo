94260b3cd99174f42e700ab0924cd38730b2ec48
Reason.H.Duan <Reason.H.Duan@newegg.com>
2017-08-09 18:23:48 +0800

    add new function set get exist in curator util

--- ZookeeperDemo/src/main/java/com/reason/util/ZookeeperCurator.java
+++ ZookeeperDemo/src/main/java/com/reason/util/ZookeeperCurator.java
@@ -4,6 +4,8 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.log4j.Logger;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
@@ -11,7 +13,7 @@ public class ZookeeperCurator {
 
     private static Logger logger = Logger.getLogger(ZookeeperCurator.class);
 
-
+    @Value("${zookeeper.hosts}")
     private String zookeeperHostPort;
 
     private CuratorFramework client = null;
@@ -23,12 +25,44 @@ public class ZookeeperCurator {
     }
 
 
+    public String getData(String path) throws Exception{
+        byte[] val = client.getData().forPath(path);
+        return val == null ? null :new String(val);
+    }
+
+    public void setData(String path, String data) throws Exception{
+        client.setData().forPath(path, data.getBytes());
+    }
+
+
+    public boolean exist(String path) throws Exception{
+        Stat stat = client.checkExists().forPath(path);
+        return stat != null;
+    }
+
+
+
+
     public static void main(String[] args) throws Exception {
+
+        String path = "/apps/wishlist_reason/reason/no";
 
         CuratorFramework client = CuratorFrameworkFactory.newClient("10.16.238.82:2181", new ExponentialBackoffRetry(10000, 10));
         client.start();
 
-        List<String> childrens = client.getChildren().forPath("/");
+//        List<String> childrens = client.getChildren().forPath(path);
+//        System.out.println("ChildList:");
+//        childrens.forEach(a->{System.out.println(a);});
+
+//        byte[] val = client.getData().forPath(path);
+//        System.out.println("Data:"+new String(val));
+
+//        client.create().forPath(path);
+//        client.setData().forPath(path, "123456".getBytes());
+//        byte[] val = client.getData().forPath(path);
+//        System.out.println("Data:"+new String(val));
+        Stat stat = client.checkExists().forPath(path);
+        System.out.println(stat);
 
     }
 
